0285364a60f325fad8acc5b6700c731486ec5c30
Marcelo Luiz Pinheiro <marcelo.pinheiro@alunos.sc.senac.br>
2025-03-28 13:53:00 -0300

    feat: add publish

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -48,3 +48,41 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "com.havan.paladinos.business-logic.kmp"
+            artifactId = "havan-paladinos-packages"
+            version = "1.0.4"
+
+            afterEvaluate {
+                from(components["kotlin"])
+            }
+
+            pom {
+                name = "ToDo Kit"
+                description =
+                    "Biblioteca KMP para gerenciamento de tarefas, permitindo adicionar, editar, remover e visualizar tarefas com status e histórico."
+                inceptionYear = "2025"
+                url = "https://github.com/MarceloLuiz12/TodoKit"
+                licenses {
+                    license {
+                        name = "MIT"
+                        url = "https://opensource.org/licenses/MIT"
+                    }
+                }
+                developers {
+                    developer {
+                        name = "Marcelo Luiz Pinheiro"
+                        email = "marceloluizpinheiro17@gmail.com"
+                    }
+                }
+                scm {
+                    url =
+                        "https://github.com/MarceloLuiz12/TodoKit"
+                }
+            }
+        }
+    }
+}
