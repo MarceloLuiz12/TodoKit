92791a33292f3f33126fabb2b628bff9a5c14d7a
Marcelo Luiz Pinheiro <106930566+MarceloLuiz12@users.noreply.github.com>
2025-04-01 09:24:57 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
       .binaryTarget(
          name: "shared",
          url: "https://github.com/MarceloLuiz12/TodoKit.git",
-         checksum: "b5f7637f1aa081f6c74a273685e69ce0f12230a3470d10e6bc6675b9ac54202a"
+         checksum: "591ffad29f84d6f1e9a0cf85de5c256ed68edfd391ce3d5f5b0e113c1b6742c7"
       )
    ]
 )
