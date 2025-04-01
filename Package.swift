57b35b80f1551ee408546a51722120f7f08a790c
Marcelo Luiz Pinheiro <106930566+MarceloLuiz12@users.noreply.github.com>
2025-04-01 09:46:48 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "shared",
-         url: "https://github.com/MarceloLuiz12/TodoKit.git",
+         url: "https://github.com/MarceloLuiz12/TodoKit/releases/download/1.0.2/XCFrameworks.zip",
          checksum: "7dbda237932d7ef38e180d7534c94c66438dde1dae792bd97636e4fc52b5653f"
       )
    ]
